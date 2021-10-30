55948574ff31997222bc8963d11903df70f767b4
sirben181 <bensonkamauben@gmail.com>
2021-10-30 22:18:54 +0300

    more

--- src/components/LeftSideBar.js
+++ src/components/LeftSideBar.js
@@ -5,15 +5,34 @@ import '../index.css'
 const LeftSideBar = () => {
     return (
         <div className="wrapper">
-            <div className="content-container">
                 <div className="left-sidebar">
+                <div className="content-container">
                     <div className="left-profile-flex">
-                      <image src={prof} className="left-profile-flex" /> <h5>Benson Kamau</h5>
+                      <image src={prof} className="left-profile-link" /> <h5>Benson Kamau</h5>
+                    </div>
+                    <div className="friends">
+                        <a href="">Friends</a>
+                    </div>
+                    <div className="watch">
+                        <a href="">Watch</a>
                     </div>
                     <div className="groups">
-                        
+                        <a href="">Groups</a>
+                    </div>
+                    <div className="market">
+                        <a href="">Marketplace</a>
+                    </div>
+                    <div className="saved">
+                        <a href="">saved</a>
                     </div>
 
+                </div>
+                <div className="shortcuts">
+                    <h1>YOUR SHORTCUTS</h1>
+                  <a href="">Familia ya Matangazo ya mpira</a>
+                  <a href=""> Kiamariga primary</a>
+                  <a href=""> Biochemistry</a>
+                  <a href="">Chuka university</a>
                 </div>
              </div>
         </div>
