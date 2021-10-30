94cce9e48fed068bff7a41a3fd10c36a1ef0d548
sirben181 <bensonkamauben@gmail.com>
2021-10-30 22:09:00 +0300

    a column

--- src/components/LeftSideBar.js
+++ src/components/LeftSideBar.js
@@ -4,13 +4,18 @@ import '../index.css'
 
 const LeftSideBar = () => {
     return (
-        <div class="wrapper">
-            <div class="content-container">
-                <div class="left-sidebar">
-                    <image src={prof} />
+        <div className="wrapper">
+            <div className="content-container">
+                <div className="left-sidebar">
+                    <div className="left-profile-flex">
+                      <image src={prof} className="left-profile-flex" /> <h5>Benson Kamau</h5>
+                    </div>
+                    <div className="groups">
+                        
+                    </div>
 
                 </div>
-            </div>
+             </div>
         </div>
     )
 }
