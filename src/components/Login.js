8412ad05757ab1b29b1533585735e90f62b22e48
sirben181 <bensonkamauben@gmail.com>
2021-11-11 15:34:48 +0300

    more function

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,11 +2,15 @@ import React from 'react'
 import {FaFacebookSquare} from 'react-icons/fa'
 import './Login.css'
 const Login = () => {
+    const signIn=()=>{
+        console.log('signed in')
+    }
+    
     return (
         <div className="container"> 
          <div className="Logincard">
          <FaFacebookSquare className="fb-icon" />
-        <button type='text' 
+        <button type='submit' onClick={signIn}
     className="btnSignin">SignIn</button>
             
          </div>
---
+++ src/components/firebase.js
@@ -0,0 +1,30 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyByo-Ibk-hn-lRW5NUX6KMya2Wc5xrOfpk",
+    authDomain: "fb-react-version.firebaseapp.com",
+    projectId: "fb-react-version",
+    storageBucket: "fb-react-version.appspot.com",
+    messagingSenderId: "884197277505",
+    appId: "1:884197277505:web:4528de85ea63577ae7ebc0",
+    measurementId: "G-7VE21YTR0H"
+  };
+// Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyByo-Ibk-hn-lRW5NUX6KMya2Wc5xrOfpk",
+//   authDomain: "fb-react-version.firebaseapp.com",
+//   projectId: "fb-react-version",
+//   storageBucket: "fb-react-version.appspot.com",
+//   messagingSenderId: "884197277505",
+//   appId: "1:884197277505:web:4528de85ea63577ae7ebc0",
+//   measurementId: "G-7VE21YTR0H"
+// };
+
+// Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
